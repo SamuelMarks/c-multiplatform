35b15d0cba51ae79ec8dd5cc15927a92b6206744
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-08-20 16:36:19 +1200

    CI fixes

--- tests/test_example_auth_flow.c
+++ tests/test_example_auth_flow.c
@@ -23,7 +23,7 @@ extern int g_mock_dlopen_fail;
 int main(void) {
     int i;
     printf("Running OOM loop for auth_flow...\n");
-    for (i = 1; i < 500; i++) {
+    for (i = 1; i < 30; i++) {
         g_malloc_called = 0;
         g_malloc_fail_countdown = i;
         example_auth_flow_main();
